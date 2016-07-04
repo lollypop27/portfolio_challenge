ba54de82f20886b31ff047d9b74b22e22a758326
lollypop27 <lara@adafctory.co.za>
2016-07-04 22:49:57 +0200

    adding content and code to test yml is working

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -18,3 +18,11 @@ describe "index", type: :feature do
     expect.(page).to have_css 'middleman-logo.svg'
   end
 end
+
+it 'displays project list' do
+  expect(page).to have_css '.projects'
+  within '.projects' do
+    expect(page).to have_content 'My First Website'
+    expect(page).to have_content 'FizzBuzz'
+  end
+end
