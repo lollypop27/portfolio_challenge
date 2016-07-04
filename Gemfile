296a72f2b29a9e55ead2b4cd838664a437443ad2
lollypop27 <lara@adafctory.co.za>
2016-07-04 22:04:05 +0200

    setting up middleman

---
+++ Gemfile
@@ -0,0 +1,19 @@
+
+source 'https://rubygems.org'
+
+# Middleman Gems
+gem 'middleman', '>= 4.0.0'
+gem 'middleman-livereload'
+gem 'middleman-sprockets', '~> 4.0.0.rc'
+gem 'middleman-deploy', '~> 2.0.0.pre.alpha'
+
+#Template engine
+gem 'haml'
+
+group :development, :test do
+  gem 'capybara'
+  gem 'capybara-webkit'
+  gem 'pry'
+  gem 'launchy'
+  gem 'rspec'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+spec_helper.rb
+require 'rspec'
+require 'capybara/rspec'
+
+require 'middleman-core'
+require 'middleman-core/rack'
+require 'middleman-livereload'
+require 'middleman-deploy'
+
+middleman_app = ::Middleman::Application.new
+
+Capybara.app = ::Middleman::Rack.new(middleman_app).to_app do
+  set :root, File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  set :environment, :development
+  set :show_exceptions, false
+end
